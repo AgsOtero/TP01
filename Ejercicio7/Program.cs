1d7b28d756250889d6b520c3c8454e012e444b73
agustin otero <oteroagustin95@gmail.com>
2019-08-16 22:58:13 -0300

    Resueltos 9 y 10

--- Ejercicio7/Program.cs
+++ Ejercicio7/Program.cs
@@ -12,16 +12,19 @@ namespace Ejercicio7
         {
             double resultadolebniz = 0;
             double porcentaje = 0;
-
-            for (int n =0; n < 1000000; n++)
+            Console.WriteLine("Ingrese la cantidad de terminos: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+            
+            for (int n=j; n>=0; n--)
             {
                 resultadolebniz = resultadolebniz + ((Math.Pow(-1, n)) / (2 * n + 1));
             }
             resultadolebniz = resultadolebniz * 4;
-            porcentaje = ((resultadolebniz * 100) / Math.PI) ;
+            porcentaje = 100 -((resultadolebniz * 100) / Math.PI) ;
             Console.WriteLine("El resultado de la formula de Lebniz es: " +resultadolebniz);
-            Console.WriteLine("El resultado respecto a la constante en terminos porcentuales es: "+porcentaje+"%");
+            Console.WriteLine("El resultado respecto a la constante en terminos porcentuales es: "+Math.Round(porcentaje,8)+"%");
 
         }
+        
     }
 }
--- Ejercicio9/Program.cs
+++ Ejercicio9/Program.cs
@@ -10,8 +10,39 @@ namespace Ejercicio9
     {
         static void Main(string[] args)
         {
-            int a = 0, i, n; // a es la cantidad de divisores, i en este caso será
-                
+            int a = 0; // a es la cantidad de divisores
+            int d = 0; //que usamos de divisor
+            int total = 0; //total sumatoria
+
+            for (int n = 35; n <= 1977; n++) //recorre de 35 hasta 1977
+            {
+                do
+                {
+                    d++;
+                    if (n % d == 0)//Si el resto de la division es 0, aumento en 1 la cantidad de divisores
+                    {
+                        a++;
+                    }
+                }
+                while ((d <= n) && (a <= 2)); // mientras el divisor no supere a 35 y la cta de divisores sea menor a 2.
+                    if (a != 2)//No es primo, reseteo contadores y paso al proximo numero
+                    {
+                        d = 0;
+                        a = 0;
+                    }
+                    else//es primo
+                    {
+                        total = total + n;
+                        d = 0;
+                        a = 0;
+                    }
+                }
+            Console.WriteLine("El total de la sumatoria es: "+total);
+        }    
         }
     }
-}
+
+
+                
+
+
